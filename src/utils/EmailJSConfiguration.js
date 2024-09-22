b2b412afe33d356880b207ccc76802ec31ee37ff
mohammad-kawach <mohmad.kawach.777@gmail.com>
2024-09-22 10:38:42 +0200

    updating .ignore

---
+++ src/utils/EmailJSConfiguration.js
@@ -0,0 +1,6 @@
+export const EMAILJS_CONFIGURATION = {
+  SERVICE_ID: "service_nm7zfmy",
+  TEMPLATE_ID: "template_in2a5cj",
+  USER_ID: "Waiya8XK4ra9Uh58K",
+  EMAIL_REGEX: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+}
