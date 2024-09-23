cf97e2d813d6e8bce7d6d3e1485e00898bd1565f
mohammad-kawach <mohmad.kawach.777@gmail.com>
2024-09-23 13:18:30 +0200

    email update

--- src/utils/EmailJSConfiguration.js
+++ src/utils/EmailJSConfiguration.js
@@ -1,6 +1,9 @@
 export const EMAILJS_CONFIGURATION = {
-  SERVICE_ID: "service_nm7zfmy",
-  TEMPLATE_ID: "template_in2a5cj",
-  USER_ID: "Waiya8XK4ra9Uh58K",
+  // SERVICE_ID: "service_nm7zfmy",
+  SERVICE_ID: "service_5mefqki",
+  // TEMPLATE_ID: "template_in2a5cj",
+  TEMPLATE_ID: "template_c3tvrr9",
+  // USER_ID: "Waiya8XK4ra9Uh58K",
+  USER_ID: "Aum3ED1xsoqLKZNxX",
   EMAIL_REGEX: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
 }
